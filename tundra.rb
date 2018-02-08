5d6e7a559c1463af478f849b441990f7d327fed1
Kristopher Johnson <kris@kristopherjohnson.net>
2018-02-08 06:49:31 -0500

    Tweak attack/release and amplitude in tundra.rb

--- tundra.rb
+++ tundra.rb
@@ -4,42 +4,42 @@
 # Inspired by Thomas Köner's early albums, especially "Permafrost"
 
 live_loop :drone do
-  rate = rrand(0.21, 0.24)
+  rate = rrand(0.20, 0.24)
   pan = rrand(-0.3, -0.3)
   
-  sample :ambi_drone, attack: 3, rate: rate, pan: pan
+  sample :ambi_drone, attack: 3, release: 3, rate: rate, pan: pan, amp: 1.5
   
-  sleep 13.1
+  sleep 10 + rrand(0.0, 5.0)
 end
 
 live_loop :dark_whoosh do
-  sleep 17 + rrand(0, 5)
+  sleep 23 + rrand(0, 10)
   
   rate = rrand(0.20, 0.35)
-  pan = rrand(-0.8, 0.8)
-  amp = rrand(0.5, 0.9)
+  pan = rrand(-0.9, 0.9)
+  amp = rrand(0.5, 1.0)
   
-  sample :ambi_dark_woosh, rate: rate, pan: pan, amp: amp, attack: 1, release: 1
+  sample :ambi_dark_woosh, rate: rate, pan: pan, amp: amp, attack: 2, release: 2
 end
 
 live_loop :swoosh do
-  sleep 29 + rrand(0, 5)
+  sleep 29 + rrand(0, 10)
   
   rate = rrand(0.30, 0.45)
-  pan = rrand(-0.8, 0.8)
-  amp = rrand(0.5, 0.9)
+  pan = rrand(-0.9, 0.9)
+  amp = rrand(0.5, 1.0)
   
-  sample :ambi_swoosh, rate: rate, pan: pan, amp: amp, attack: 1, release: 1
+  sample :ambi_swoosh, rate: rate, pan: pan, amp: amp, attack: 2, release: 2
 end
 
 live_loop :choir do
-  sleep 7 + rrand(0, 3)
+  sleep 7 + rrand(0, 5)
   
-  rate = choose([0.05, 0.067, 0.1, 0.133])
+  rate = rrand(0.05, 0.12)
   pan = rrand(-0.5, 0.5)
-  amp = rrand(0.5, 0.9)
+  amp = rrand(0.7, 1.0)
   
-  sample :ambi_choir, rate: rate, pan: pan, amp: amp
+  sample :ambi_choir, rate: rate, pan: pan, amp: amp, attack: 2, release: 3
 end
 
 live_loop :glitch do
@@ -53,6 +53,6 @@ live_loop :glitch do
     
     pan = rrand(-0.9, 0.9)
     
-    sample sample_name, rate: rate, pan: pan, attack: 0.5, release: 0.5, amp: 0.66
+    sample sample_name, rate: rate, pan: pan, attack: 0.5, release: 1, amp: 0.75
   end
 end
